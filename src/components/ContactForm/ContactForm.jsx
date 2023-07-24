0241e2183e1a52d094b7160e97647a22a038998f
SofiiaNikolenko <sofii.nikolenko@gmail.com>
2023-07-24 23:09:36 +0200

    some chamges

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,59 +1,152 @@
+// import React, { useState } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { addContactThunk } from '../../redux/contacts/operations';
+// import { contactsSelector } from '../../redux/contacts/selectors';
+
+// const ContactForm = () => {
+//   const [formData, setFormData] = useState({ name: '', phone: '' });
+//   const [formErrors, setFormErrors] = useState({ name: '', phone: '' });
+//   const contacts = useSelector(contactsSelector);
+//   const dispatch = useDispatch();
+
+//   const handleChange = event => {
+//     const { name, value } = event.target;
+//     setFormData(prevFormData => ({ ...prevFormData, [name]: value }));
+//     setFormErrors(prevFormErrors => ({ ...prevFormErrors, [name]: '' }));
+//   };
+
+//   const handleSubmit = event => {
+//     event.preventDefault();
+
+//     const { name: newName, phone } = formData;
+
+//     if (contacts.some(({ name }) => newName === name)) {
+//       alert(`${newName} is already in contacts`);
+//       return;
+//     }
+
+//     dispatch(addContactThunk({ name: newName, phone: phone.toString() }));
+
+//     setFormData({ name: '', phone: '' });
+//   };
+
+//   return (
+//     <form onSubmit={handleSubmit} autoComplete="off">
+//       <label>
+//         Name
+//         <input
+//           type="text"
+//           name="name"
+//           value={formData.name}
+//           onChange={handleChange}
+//         />
+//       </label>
+//       {formErrors.name && <span>{formErrors.name}</span>}
+
+//       <label>
+//         Number
+//         <input
+//           type="text"
+//           name="phone"
+//           value={formData.phone}
+//           onChange={handleChange}
+//         />
+//       </label>
+
+//       {formErrors.phone && <span>{formErrors.phone}</span>}
+//       <button type="submit">Add contact</button>
+//     </form>
+//   );
+// };
+
+// export default ContactForm;
+
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { addContactThunk } from '../../redux/contacts/operations';
 import { contactsSelector } from '../../redux/contacts/selectors';
 
+const phoneRegExp =
+  /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
+
 const ContactForm = () => {
-  const [formData, setFormData] = useState({ name: '', phone: '' });
-  const [formErrors, setFormErrors] = useState({ name: '', phone: '' });
   const contacts = useSelector(contactsSelector);
   const dispatch = useDispatch();
 
-  const handleChange = event => {
-    const { name, value } = event.target;
-    setFormData(prevFormData => ({ ...prevFormData, [name]: value }));
-    setFormErrors(prevFormErrors => ({ ...prevFormErrors, [name]: '' }));
+  const [formValues, setFormValues] = useState({
+    name: '',
+    phone: '',
+  });
+
+  const [formErrors, setFormErrors] = useState({
+    name: '',
+    phone: '',
+  });
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
   };
 
-  const handleSubmit = event => {
-    event.preventDefault();
+  const handleSubmit = e => {
+    e.preventDefault();
+    const { name, phone } = formValues;
 
-    const { name: newName, phone } = formData;
-
-    if (contacts.some(({ name }) => newName === name)) {
-      alert(`${newName} is already in contacts`);
+    if (contacts.some(contact => contact.name === name)) {
+      alert(`${name} is already in contacts`);
       return;
     }
 
-    dispatch(addContactThunk({ name: newName, phone: phone.toString() }));
+    if (!name || !phone) {
+      setFormErrors({
+        name: name ? '' : 'Name should not be empty',
+        phone: phone ? '' : 'Phone number should not be empty',
+      });
+      return;
+    }
 
-    setFormData({ name: '', phone: '' });
+    if (!phoneRegExp.test(phone)) {
+      setFormErrors({
+        ...formErrors,
+        phone: 'Phone number is not valid',
+      });
+      return;
+    }
+
+    dispatch(addContactThunk({ name, number: phone.toString() }));
+    setFormValues({ name: '', phone: '' });
   };
 
   return (
-    <form onSubmit={handleSubmit} autoComplete="off">
-      <label>
-        Name
-        <input
-          type="text"
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
-        />
-      </label>
-      {formErrors.name && <span>{formErrors.name}</span>}
+    <form autoComplete="off" onSubmit={handleSubmit}>
+      <div>
+        <label>
+          Name
+          <input
+            type="text"
+            name="name"
+            value={formValues.name}
+            onChange={handleChange}
+          />
+          {formErrors.name && <span>{formErrors.name}</span>}
+        </label>
+      </div>
 
-      <label>
-        Number
-        <input
-          type="text"
-          name="phone"
-          value={formData.phone}
-          onChange={handleChange}
-        />
-      </label>
-
-      {formErrors.phone && <span>{formErrors.phone}</span>}
+      <div>
+        <label>
+          Number
+          <input
+            type="tel"
+            name="phone"
+            value={formValues.phone}
+            onChange={handleChange}
+          />
+          {formErrors.phone && <span>{formErrors.phone}</span>}
+        </label>
+      </div>
       <button type="submit">Add contact</button>
     </form>
   );
